136321c081a3ea4fe570df769e24a305fab5b6ee
akash98094-tech <akash98094@gmail.com>
2026-02-28 00:38:43 +0500

    Update help.js

--- RDX/commands/help.js
+++ RDX/commands/help.js
@@ -94,7 +94,7 @@ function showDefaultHelp({ api, event, send, client, config }) {
 
   msg += `────────────────
    💡 Use ${config.PREFIX}help all (Full List)
-   👤 Owner: SARDAR RDX
+   👤 Owner: MEHAK JUTTI
 ────────────────`;
 
   return send.reply(msg);
